63fee1fb5fa5026720b6e64bb1bde73b4339fa0e
ilfan <ilfanmuratov@gmail.com>
2021-11-04 00:11:21 +0500

    Some small chages

--- src/App.vue
+++ src/App.vue
@@ -8,5 +8,15 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
+html {
+    font-size: 62.5%;
+}
+* {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+    font-family: Inter;
+    font-size: 1.4rem;
+}
 </style>
---
+++ src/components/RightPanel.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="right-panel"></div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+.right-panel {
+    position: fixed;
+    top: 0;
+    right: 0;
+    width: 370px;
+    height: 100%;
+    background: teal;
+}
+</style>
