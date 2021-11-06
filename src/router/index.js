45c22a68d7a877e6fec7c5cf1fcc81c92cf30409
ilfan <ilfanmuratov@gmail.com>
2021-11-06 12:06:01 +0500

    Login route

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from '../views/Main.vue';
+import Login from '../views/LoginPage.vue';
 
 const routes = [
 	{
 		path: '/',
 		name: 'Home',
 		component: Home,
+	},
+	{
+		path: '/login',
+		name: 'Login',
+		component: Login,
 	},
 	{
 		path: '/courses',
@@ -33,6 +39,19 @@ const routes = [
 const router = createRouter({
 	history: createWebHistory(process.env.BASE_URL),
 	routes,
+});
+
+router.beforeEach((to, from, next) => {
+	// redirect to login page if not logged in and trying to access a restricted page
+	const publicPages = ['/login'];
+	const authRequired = !publicPages.includes(to.path);
+	const loggedIn = localStorage.getItem('user');
+
+	if (authRequired && !loggedIn) {
+		return next('/login');
+	}
+
+	next();
 });
 
 export default router;
