4a958585b82fd40ece6efc70707869c8a0902c9c
ilfan <ilfanmuratov@gmail.com>
2021-11-06 12:03:34 +0500

    Global variables

--- src/store/coursesModule.js
+++ src/store/coursesModule.js
@@ -14,7 +14,7 @@ export const coursesModule = {
 		async fetchCourses({ state, commit }) {
 			try {
 				const response = await axios.get(
-					'http://localhost:8000/api/v1/courses/'
+					process.env.VUE_APP_API_URL + 'api/v1/courses/'
 				);
 				commit('setCourses', response.data);
 			} catch (e) {
