30aa5f273e41617300bcc87e9ed7dd381ea2c61d
ilfan <ilfanmuratov@gmail.com>
2021-11-06 12:05:36 +0500

    New store modules

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,11 @@
 import { createStore } from 'vuex';
 import { coursesModule } from './coursesModule';
+import { authModule } from './authModule';
+import { alertModule } from './alertModule';
 
 export default createStore({
 	state: {},
 	mutations: {},
 	actions: {},
-	modules: { courses: coursesModule },
+	modules: { courses: coursesModule, auth: authModule, alert: alertModule },
 });
