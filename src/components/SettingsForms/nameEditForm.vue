8d5603cc2ae1fca34069fd2e850de63e2928b810
ilfan <ilfanmuratov@gmail.com>
2021-12-08 20:09:12 +0500

    .

--- src/components/SettingsForms/nameEditForm.vue
+++ src/components/SettingsForms/nameEditForm.vue
@@ -1,5 +1,19 @@
 <template>
-    <form class="name-form"></form>
+    <div class="name-form">
+        <div class="name-form__heading">Имя</div>
+        <div class="name-form__body">
+            <form class="name-form__form">
+                <div class="name-form__item">
+                    <label for="first_name"></label>
+                    <input type="text" id="first_name" />
+                </div>
+                <div class="name-form__item">
+                    <label for="last_name"></label>
+                    <input type="text" id="last_name" />
+                </div>
+            </form>
+        </div>
+    </div>
 </template>
 
 <script>
