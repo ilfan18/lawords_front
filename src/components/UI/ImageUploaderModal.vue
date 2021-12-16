a8e53315cc3a0deafa4b721d68b54fab619e9d20
ilfan <ilfanmuratov@gmail.com>
2021-12-16 21:30:32 +0500

    .

--- src/components/UI/ImageUploaderModal.vue
+++ src/components/UI/ImageUploaderModal.vue
@@ -55,7 +55,6 @@ export default {
     height: 100vh;
     top: 0;
     left: 0;
-    background: rgba(255, 255, 255, 0.75);
     backdrop-filter: blur(2px);
     display: flex;
     align-items: center;
