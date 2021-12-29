4924909edb83b409d5105575399d903f14aae786
ilfan <ilfanmuratov@gmail.com>
2021-12-30 00:37:05 +0500

    Lesson info on work

--- src/views/LessonInfo.vue
+++ src/views/LessonInfo.vue
@@ -2,8 +2,15 @@
     <div class="lesson-info content">
         <v-header>Текущий курс</v-header>
         <div v-if="!isLessonLoading" class="lesson-info__content">
-            <div class="lesson-info__top"></div>
-            <div v-html="lesson.main_text" class="lesson-info__bottom"></div>
+            <div class="lesson-info__top">
+                <div class="lesson-info__rigth">
+                    <div v-html="lesson.top_text" class="lesson-info__top-text"></div>
+                </div>
+                <div class="lesson-info__left"></div>
+            </div>
+            <div class="lesson-info__bottom">
+                <div v-html="lesson.main_text" class="lesson-info__main-text"></div>
+            </div>
         </div>
         <page-loader v-else />
     </div>
@@ -35,4 +42,15 @@ export default {
 </script>
 
 <style  lang="scss" scoped>
+.lesson-info {
+    &__content {
+        font-size: 16px;
+    }
+
+    &__top {
+    }
+
+    &__bottom {
+    }
+}
 </style>
